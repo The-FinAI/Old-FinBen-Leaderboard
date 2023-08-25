5d6311b37d710466102fb51bcedf8816b2a2b49f
Jimin Huang <jimin@chancefocus.com>
2023-08-25 12:17:39 -0400

    fix bugs

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+# matplotlib.use('macosx')
 import gradio as gr
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 from apscheduler.schedulers.background import BackgroundScheduler
 
 COLS = [
@@ -30,6 +33,7 @@ COLS_AUTO = [
     ("FiQA-SA-missing", "number"),
     ("Headline-AvgF1", "number"),
     ("NER-EntityF1", "number"),
+    ("ConvFinQA-EmAcc", "number"),
     ("FinQA-EmAcc", "number"),
     ("BigData22-Acc", "number"),
     ("BigData22-MCC", "number"),
@@ -84,9 +88,13 @@ COLS_AUTO = [
     ("ECTSUM-rouge1", "number"),
     ("ECTSUM-rouge2", "number"),
     ("ECTSUM-rougeL", "number"),
+    ("ECTSUM-BertScore", "number"),
+    ("ECTSUM-BARTScore", "number"),
     ("EDTSUM-rouge1", "number"),
     ("EDTSUM-rouge2", "number"),
     ("EDTSUM-rougeL", "number"),
+    ("EDTSUM-BertScore", "number"),
+    ("EDTSUM-BARTScore", "number"),
 ]
 TYPES = [col_type for _, col_type in COLS]
 TYPES_AUTO = [col_type for _, col_type in COLS_AUTO]
@@ -97,12 +105,13 @@ cols_auto = [col_name for col_name, _ in COLS_AUTO]
 
 # Load leaderboard data with column names
 leaderboard_df = pd.read_csv('leaderboard.csv', names=cols)
-leaderboard_auto_df = pd.read_csv('leaderboard_auto.csv', names=cols_auto)
+leaderboard_auto_df = pd.read_csv('present_result.csv', names=cols_auto)
 common_cols = list(set(cols) & set(cols_auto))
 
 # Merge dataframes and replace NaN values with an empty string
-merged_df = pd.merge(
-    leaderboard_df, leaderboard_auto_df, how="outer", on=common_cols).fillna("")
+# merged_df = pd.merge(
+#     leaderboard_df, leaderboard_auto_df, how="outer", on=common_cols).fillna("")
+merged_df = leaderboard_auto_df
 
 merged_df = merged_df.sort_index(axis=1)
 
@@ -121,21 +130,31 @@ df_stock_movement_prediction = merged_df[["Model", "BigData22-Acc",
 df_ner = merged_df[["Model", "NER-EntityF1", "FinerOrd-EntityF1", "FinerOrd-F1"]]
 df_credit_scoring = merged_df[["Model", "German-Acc", "German-MCC",
     "German-missing", "Australian-Acc", "Australian-MCC", "Australian-missing"]]
-df_number_understanding = merged_df[["Model", "FinQA-EmAcc"]]
+df_number_understanding = merged_df[["Model", "FinQA-EmAcc", "ConvFinQA-EmAcc"]]
+df_text_summarization = merged_df[["Model", "ECTSUM-rouge1", "ECTSUM-rouge2",
+    "ECTSUM-rougeL", "ECTSUM-BertScore", "ECTSUM-BARTScore",
+    "EDTSUM-rouge1", "EDTSUM-rouge2", "EDTSUM-rougeL", "EDTSUM-BertScore", "EDTSUM-BARTScore",]]
 
 
 df_dict = {
     "Sentiment Analysis": df_sentiment_analysis,
-    "Stock Movement Prediction": df_stock_movement_prediction,
     "NER": df_ner,
-    "Credit Scoring": df_credit_scoring,
     "Number Understanding": df_number_understanding,
+    "Text Summarization": df_text_summarization,
+    "Stock Movement Prediction": df_stock_movement_prediction,
+    "Credit Scoring": df_credit_scoring,
 }
 
 
 # Constants
-TITLE = "Financial Natural Language Understanding and Prediction Evaluation Benchmark (FLARE) Leaderboard"
-INTRODUCTION_TEXT = "The leaderboard shows the performance of various models in financial natural language understanding and prediction tasks."
+TITLE = '<h1 align="center" id="space-title">🐲 PIXIU FLARE Leaderboard</h1>'
+# TITLE = "Financial Natural Language Understanding and Prediction Evaluation Benchmark (FLARE) Leaderboard"
+INTRODUCTION_TEXT = """📊 The PIXIU FLARE Leaderboard is designed to rigorously track, rank, and evaluate state-of-the-art models in financial Natural Language Understanding and Prediction. 
+📈 Unique to FLARE, our leaderboard not only covers standard NLP tasks but also incorporates financial prediction tasks such as stock movement and credit scoring, offering a more comprehensive evaluation for real-world financial applications.
+📚 Our evaluation metrics include, but are not limited to, Accuracy, F1 Score, ROUGE score, BERTScore, and Matthews correlation coefficient (MCC), providing a multidimensional assessment of model performance.
+🔗 For more details, refer to our GitHub page [here](https://github.com/ChanceFocus/PIXIU).
+"""
+
 
 def create_data_interface(df):
     headers = df.columns
@@ -149,37 +168,33 @@ def create_data_interface(df):
     )
 
 def plot_radar_chart(df, attributes, category_name):
-    N = len(attributes)
+    fig = go.Figure()
 
-    # Compute the angle of each axis in the plot
-    angles = [n / float(N) * 2 * np.pi for n in range(N)]
-    angles += angles[:1]
+    for index, row in df.iterrows():
+        model = row['Model']
+        values = row[attributes].tolist()
+        fig.add_trace(go.Scatterpolar(
+            r=values,
+            theta=attributes,
+            fill='toself',
+            name=model
+        ))
 
-    # Create a radar/spider chart
-    plt.figure(figsize=(10, 7))
-    ax = plt.subplot(111, polar=True)
-    ax.set_theta_offset(np.pi / 2)
-    ax.set_theta_direction(-1)
-    
-    # Set labels
-    plt.xticks(angles[:-1], attributes, color='grey', size=10)
+    fig.update_layout(
+        title="FLARE",
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+                range=[0, 0.9]
+            )),
+        showlegend=True
+    )
 
-    # Plot line for each model
-    for _, row in df.iterrows():
-        model_name = row['Model']
-        values = row.drop('Model').values.tolist()
-        values += values[:1]
-        ax.plot(angles, values, linewidth=2, linestyle='solid', label=model_name)
-        ax.fill(angles, values, alpha=0.1)
-
-    ax.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
-    plt.title(category_name, size=20, color='black', y=1.1)
-    plt.close()
-
-    return plt
+    return fig
 
 def create_data_interface_for_aggregated(df, category_name):
     attributes = df.columns[1:]
+    print (attributes)
     plt = plot_radar_chart(df, attributes, category_name)
     return plt
 
@@ -189,12 +204,28 @@ def launch_gradio():
     with demo:
         gr.HTML(TITLE)
         gr.Markdown(INTRODUCTION_TEXT, elem_classes="markdown-text")
+        new_df = pd.DataFrame()
+        for key, df in df_dict.items():
+            new_df["Model"] = df["Model"]
+            tdf = df.replace('', 0)
+            if key == "Sentiment Analysis":
+                tdf = tdf[[val for val in tdf.columns if "F1" in val]]
+            elif key == "Stock Movement Prediction":
+                tdf = tdf[[val for val in tdf.columns if "Acc" in val]]
+            elif key == "Credit Scoring":
+                tdf = tdf[[val for val in tdf.columns if "Acc" in val]]
+            elif key == "Text Summarization":
+                tdf = tdf[[val for val in tdf.columns if "Bert" in val]]
+            print (tdf)
+            new_df[key] = tdf.values[:, 1:].mean(axis=1)
+        print (new_df)
+
+        plot = create_data_interface_for_aggregated(new_df, key)
+        gr.Plot(plot)
 
         for key, df in df_dict.items():
             with gr.Tab(key):
                 create_data_interface(df)
-                plot = create_data_interface_for_aggregated(df, key)
-                gr.Plot(plot)
 
     demo.launch()
 
